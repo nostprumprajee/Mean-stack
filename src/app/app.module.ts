550bdd4854d3a0d55616402a8a71beecbc61d37e
nostprumprajee <nostprumprajee@gmail.com>
2019-04-25 12:36:28 +0700

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  BrowserModule,
+  FormsModule,
+  HttpClientModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
