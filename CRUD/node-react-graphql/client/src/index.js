320e45c9058aeede74aee5bfcdd61aac9d62c353
nostprumprajee <nostprumprajee@gmail.com>
2019-04-29 17:11:53 +0700

    add components js

--- CRUD/node-react-graphql/client/src/index.js
+++ CRUD/node-react-graphql/client/src/index.js
@@ -4,8 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
+
+const client = new ApolloClient();
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/create' component={Create} />
+            <Route path='/show/:id' component={Show} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
