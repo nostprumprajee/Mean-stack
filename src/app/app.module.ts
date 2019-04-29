26448ab1d8aa68d1e5411797f229420dd2af6819
nostprumprajee <nostprumprajee@gmail.com>
2019-04-29 15:18:01 +0700

    Book component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,53 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent,
+    data: { title: 'Book List' }
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailComponent,
+    data: { title: 'Book Details' }
+  },
+  {
+    path: 'book-create',
+    component: BookCreateComponent,
+    data: { title: 'Create Book' }
+  },
+  {
+    path: 'book-edit/:id',
+    component: BookEditComponent,
+    data: { title: 'Edit Book' }
+  },
+  { path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
-  BrowserModule,
-  FormsModule,
-  HttpClientModule
-],
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
